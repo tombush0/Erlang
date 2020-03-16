2b1df6a1c8d37c7cdafd3cde689f39a4219c91a3
wokstym <grzegorz.poreba.73@gmail.com>
2020-03-16 16:59:06 +0100

    init

---
+++ src/main.erl
@@ -0,0 +1,46 @@
+%%%-------------------------------------------------------------------
+%%% @author wokstym
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 02. mar 2020 14:20
+%%%-------------------------------------------------------------------
+-module(main).
+-author("wokstym").
+
+%% API
+-export([factorial/1, main/1, power/2, contains/2, duplicateElements/1, sumFloats/1, sumFloatsTail/2]).
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
+
+main(N) -> io:format("~B~n",[factorial(N)]).
+
+power(A, 1) -> A;
+power(A, B) -> A* power(A, B-1).
+
+contains([], _) -> false;
+contains([H|_], H) -> true;
+contains([_|T], X) -> contains(T, X).
+
+
+duplicateElements([])-> [];
+duplicateElements([H|T])-> [H, H| duplicateElements(T)].
+
+
+sumFloats (L) -> sumFloatsTail(L, 0).
+
+sumFloatsTail([], SUM) -> SUM;
+sumFloatsTail([H|T], SUM) -> sumFloatsTail(T, SUM+H).
+
+ONP([H]) ->
+
+
+%%  element(1, MyTuple). --- element 1 z tupla
+%%
+%%f(). - czyści wszystkie wiązania zmiennych.
+%%time(). Zwiąż zmienną o nazwie Minutki z aktualną minutą.
+%%list_to_tuple(ListaProduktow).
+%%io:format(„Produkt w sklepie nazywa się ~p.~nAktualna minutka to ~B.~n”, [NazwaP1, Minutki] ).
+% funcja stringa to integer
